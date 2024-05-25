b103a65b389696da6a90f25944932c7dfc9a799a
LostShadow <156698581+LostShadowGD@users.noreply.github.com>
2024-05-25 15:26:59 +0100

    Update listlevel.js

--- listlevel.js
+++ listlevel.js
@@ -2,7 +2,7 @@ let _levels = 1;
 class _listLevel extends HTMLElement{
 constructor(){super();
 const template = `
-<div class="level" style="display:grid; max-height:20vh; color: #ffffff; background-color: #555555">
+<div class="level" style="display:grid; max-height:20vh; color:#ffffff; background-color:#555555;">
 <img style="object-fit: cover; height:18vh; width:100%; aspect-ratio:16/9; border-radius:5px;" src="${this.getAttribute("img")}"></img>
 <span class="level_info" style="flex:1;">
 <h2 style="font-size:3vh;">${_levels}. ${this.getAttribute("lv")}</h2>
