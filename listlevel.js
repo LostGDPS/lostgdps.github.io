f4e07d28f5700afde14647115e0ee3c73a1a5201
LostShadow <156698581+LostShadowGD@users.noreply.github.com>
2024-05-27 18:15:26 +0100

    Update listlevel.js

--- listlevel.js
+++ listlevel.js
@@ -1,21 +1,21 @@
 let _levels = 1;
 class _listLevel extends HTMLElement{
-	constructor(){super();
-		const template = `
-<div class="level" style="display:grid; max-height:11vh; width:50%;">
-	<img style="object-fit: cover; height:9vh; width:100%; aspect-ratio:16/9; border-radius:5px;" src="${this.getAttribute("img")}"></img>
-	<span class="level_info" style="flex:0;">
-		<h2 style="font-size:2vh;">${_levels}. ${this.getAttribute("lv")}</h2>
-		<p style="font-size:1vh;">${this.getAttribute("creator")}</p>
-	</span>
+constructor(){super();
+const template = `
+<div class="level" style="display:grid; width:50%;">
+<img style="object-fit: cover; width:100%; aspect-ratio:16/9; border-radius:5px;" src="${this.getAttribute("img")}"></img>
+<span class="level_info" style="flex:0;">
+<h2 style="font-size:100%;">${_levels}. ${this.getAttribute("lv")}</h2>
+<p style="font-size:75%;">${this.getAttribute("creator")}</p>
+</span>
 </div>
 <br/><br/>
-		`
-		this.innerHTML=template;
-		_levels+=1;
-	}
-	connectedCallback(){
-	}
+`
+this.innerHTML=template;
+_levels+=1;
+}
+connectedCallback(){
+}
 }
 
-window.customElements.define("list-level", _listLevel);
+window.customElements.define("list-level", _listLevel);"list-level", _listLevel);
